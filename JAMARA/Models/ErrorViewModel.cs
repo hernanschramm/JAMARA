2fd036ae2122de98424b8288729262c57ea01205
hernanschramm <schrammhernangabriel@hotmail.com>
2023-07-18 14:40:56 -0300

    Crear Proyecto JAMARA

---
+++ JAMARA/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace JAMARA.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
